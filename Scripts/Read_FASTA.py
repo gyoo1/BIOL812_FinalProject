79391bd18c9d41c1925f8c28f2bd3f227414f53d
gyoo1 <gihyun@ualberta.ca>
2022-04-18 17:41:22 -0400

    Update script

--- Scripts/Read_FASTA.py
+++ Scripts/Read_FASTA.py
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 #Combining FASTA files into single file 
 
-=======
->>>>>>> 3feb83da8228dfb076aeeebc39315bd819f32533
 import os
-DIR = 'Sequences'
+os.getcwd() #current directory
+DIR = input("\nInput folder containing FASTA files to combine : ")
 os.chdir(DIR)
-DIR = os.getcwd()
-file_name = input( "What you you like to name output file? Note: please include .fasta extension: ")
+FILE_NAME = input("Enter output file name with .fasta extension : ")
+output_fas = open(FILE_NAME, 'w')
+file_count = 0
 
-output_fasta= open(file_name, "w")
-file_count=0
-
-for f in os.listdir(DIR):
-    if f.endswith((".fasta")):
-        file_count+=1
-        fh= open(os.path.join(DIR, f))
-        for line in fh:
-            output_fasta.write(line)
+for f in os.listdir():
+        if f.endswith((".fasta")):
+                file_count += 1
+                with open(f) as fh:
+                        output_fas.writelines(fh)
         fh.close()
-
-output_fasta.close()
-print(str(file_count)) + "FASTA files have been merged into single file, can be found at " + DIR
-
+print(str(file_count)) + " FASTA files were merged into one file"
